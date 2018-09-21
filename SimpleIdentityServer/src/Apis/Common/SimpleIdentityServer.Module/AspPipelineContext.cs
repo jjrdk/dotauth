c8cd68ea809cf6bbd2f678636ba23866f1be5ddb
thabart <habarthierry@hotmail.fr>
2018-09-21 17:17:27 +0200

    Ticket 58 : Start to mod SID

---
+++ SimpleIdentityServer/src/Apis/Common/SimpleIdentityServer.Module/AspPipelineContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleIdentityServer.Module
+{
+    public class AspPipelineContext
+    {
+        private ConfigureServiceContext _configureServiceContext;
+        private static AspPipelineContext _instance;
+
+        private AspPipelineContext() { }
+
+        public static AspPipelineContext Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new AspPipelineContext();
+            }
+
+            return _instance;
+        }
+
+        public void StartConfigureServices(IServiceCollection services)
+        {
+            _configureServiceContext = new ConfigureServiceContext(services);
+        }
+
+        public ConfigureServiceContext ConfigureServiceContext
+        {
+            get
+            {
+                return _configureServiceContext;
+            }
+        }
+    }
+}
