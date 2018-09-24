3c94d4a0c09929146d9197e1f92f37d6662adacf
thierry habart <habarthierry@hotmail.fr>
2018-09-24 21:55:13 +0200

    Ticket 59 : Mod. SIMPLEBUS

--- SimpleIdentityServer/src/Apis/Scim/SimpleIdentityServer.Scim.Db.EF.Sqlite/ScimSqliteModule.cs
+++ SimpleIdentityServer/src/Apis/Scim/SimpleIdentityServer.Scim.Db.EF.Sqlite/ScimSqliteModule.cs
@@ -1,18 +1,11 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
-using SimpleIdentityServer.Module;
-using System;
+﻿using SimpleIdentityServer.Module;
 using System.Collections.Generic;
 
 namespace SimpleIdentityServer.Scim.Db.EF.Sqlite
 {
     public class ScimSqliteModule : IModule
     {
-        public void Init()
+        public void Init(IDictionary<string, string> properties)
         {
 
         }
---
+++ SimpleIdentityServer/src/Lib/Bus/SimpleBus.RabbitMq/SimpleBusRabbitMqModule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SimpleIdentityServer.Module;
+
+namespace SimpleBus.RabbitMq
+{
+    public class SimpleBusRabbitMqModule : IModule
+    {
+        public void Init(IDictionary<string, string> options)
+        {
+        }
+    }
+}
