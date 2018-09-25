036a3f2b93173ee24b744d7ae5d886756b1032d1
thierry habart <habarthierry@hotmail.fr>
2018-09-25 18:14:22 +0200

    Ticket XX : Load missing lib

--- SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.Authenticate.LoginPassword/ServiceCollectionExtensions.cs
+++ SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.Authenticate.LoginPassword/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using SimpleIdentityServer.Authenticate.Basic;
@@ -6,6 +7,7 @@ using SimpleIdentityServer.Authenticate.LoginPassword.Controllers;
 using SimpleIdentityServer.Authenticate.LoginPassword.Services;
 using SimpleIdentityServer.Core.Services;
 using System;
+using System.Reflection;
 
 namespace SimpleIdentityServer.Authenticate.LoginPassword
 {
@@ -33,6 +35,11 @@ namespace SimpleIdentityServer.Authenticate.LoginPassword
             services.Configure<RazorViewEngineOptions>(opts =>
             {
                 opts.FileProviders.Add(embeddedFileProvider);
+                opts.CompilationCallback = (context) =>
+                {
+                    var assm = MetadataReference.CreateFromFile(Assembly.Load("SimpleIdentityServer.Authenticate.Basic").Location);
+                    context.Compilation = context.Compilation.AddReferences(assm);
+                };
             });
             services.AddSingleton(basicAuthenticateOptions);
             services.AddTransient<IAuthenticateResourceOwnerService, PasswordAuthenticateResourceOwnerService>();
