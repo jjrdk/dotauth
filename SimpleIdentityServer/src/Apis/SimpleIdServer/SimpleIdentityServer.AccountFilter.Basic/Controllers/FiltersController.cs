f6127a733ab89ce1c0ba1e832d446a67e5d0ab75
Jacob Reimers <jacob@reimers.dk>
2018-10-10 21:56:16 +0200

    Remove unused code. Combine assemblies.

--- SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.AccountFilter.Basic/Controllers/FiltersController.cs
+++ SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.AccountFilter.Basic/Controllers/FiltersController.cs
@@ -1,167 +1,167 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using SimpleIdentityServer.AccountFilter.Basic.Common.Requests;
-using SimpleIdentityServer.AccountFilter.Basic.Common.Responses;
-using SimpleIdentityServer.AccountFilter.Basic.Extensions;
-using SimpleIdentityServer.AccountFilter.Basic.Repositories;
-using SimpleIdentityServer.Common.Dtos.Responses;
-using System.Net;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using SimpleIdentityServer.AccountFilter.Basic.Common.Requests;
+//using SimpleIdentityServer.AccountFilter.Basic.Common.Responses;
+//using SimpleIdentityServer.AccountFilter.Basic.Extensions;
+//using SimpleIdentityServer.AccountFilter.Basic.Repositories;
+//using SimpleIdentityServer.Common.Dtos.Responses;
+//using System.Net;
+//using System.Threading.Tasks;
 
-namespace SimpleIdentityServer.AccountFilter.Basic.Controllers
-{
-    [Route("filters")]
-    public class FiltersController : Controller
-    {
-        private readonly IFilterRepository _filterRepository;
+//namespace SimpleIdentityServer.AccountFilter.Basic.Controllers
+//{
+//    [Route("filters")]
+//    public class FiltersController : Controller
+//    {
+//        private readonly IFilterRepository _filterRepository;
 
-        public FiltersController(IFilterRepository filterRepository)
-        {
-            _filterRepository = filterRepository;
-        }
+//        public FiltersController(IFilterRepository filterRepository)
+//        {
+//            _filterRepository = filterRepository;
+//        }
 
-        #region Public methods
+//        #region Public methods
 
-        [HttpGet]
-        [Authorize("manage_account_filtering")]
-        public async Task<IActionResult> GetFilters()
-        {
-            var filters = await _filterRepository.GetAll();
-            return new OkObjectResult(filters.ToDtos());
-        }
+//        [HttpGet]
+//        [Authorize("manage_account_filtering")]
+//        public async Task<IActionResult> GetFilters()
+//        {
+//            var filters = await _filterRepository.GetAll();
+//            return new OkObjectResult(filters.ToDtos());
+//        }
 
-        [HttpGet("{id}")]
-        [Authorize("manage_account_filtering")]
-        public async Task<IActionResult> GetFilter(string id)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-            {
-                return BuildMissingParameter("id");
-            }
+//        [HttpGet("{id}")]
+//        [Authorize("manage_account_filtering")]
+//        public async Task<IActionResult> GetFilter(string id)
+//        {
+//            if (string.IsNullOrWhiteSpace(id))
+//            {
+//                return BuildMissingParameter("id");
+//            }
 
-            var filter = await _filterRepository.Get(id);
-            if (filter == null)
-            {
-                return new StatusCodeResult((int)HttpStatusCode.NotFound);
-            }
+//            var filter = await _filterRepository.Get(id);
+//            if (filter == null)
+//            {
+//                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+//            }
 
-            return new OkObjectResult(filter.ToDto());
-        }
+//            return new OkObjectResult(filter.ToDto());
+//        }
 
-        [HttpPost]
-        [Authorize("manage_account_filtering")]
-        public async Task<IActionResult> AddFilter([FromBody] AddFilterRequest addFilterRequest)
-        {
-            if (addFilterRequest == null)
-            {
-                return BuildMissingParameter(nameof(addFilterRequest));
-            }
+//        [HttpPost]
+//        [Authorize("manage_account_filtering")]
+//        public async Task<IActionResult> AddFilter([FromBody] AddFilterRequest addFilterRequest)
+//        {
+//            if (addFilterRequest == null)
+//            {
+//                return BuildMissingParameter(nameof(addFilterRequest));
+//            }
 
-            IActionResult errorResult = null;
-            if (!CheckParameter(addFilterRequest, out errorResult))
-            {
-                return errorResult;
-            }
+//            IActionResult errorResult = null;
+//            if (!CheckParameter(addFilterRequest, out errorResult))
+//            {
+//                return errorResult;
+//            }
             
-            var id = await _filterRepository.Add(addFilterRequest.ToParameter());
-            return new OkObjectResult(new AddFilterResponse
-            {
-                Id = id
-            });
-        }
+//            var id = await _filterRepository.Add(addFilterRequest.ToParameter());
+//            return new OkObjectResult(new AddFilterResponse
+//            {
+//                Id = id
+//            });
+//        }
 
-        [HttpDelete("{id}")]
-        [Authorize("manage_account_filtering")]
-        public async Task<IActionResult> DeleteFilter(string id)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-            {
-                return BuildMissingParameter("id");
-            }
+//        [HttpDelete("{id}")]
+//        [Authorize("manage_account_filtering")]
+//        public async Task<IActionResult> DeleteFilter(string id)
+//        {
+//            if (string.IsNullOrWhiteSpace(id))
+//            {
+//                return BuildMissingParameter("id");
+//            }
 
-            var filter = await _filterRepository.Get(id);
-            if (filter == null)
-            {
-                return new StatusCodeResult((int)HttpStatusCode.NotFound);
-            }
+//            var filter = await _filterRepository.Get(id);
+//            if (filter == null)
+//            {
+//                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+//            }
 
-            await _filterRepository.Delete(id);
-            return new NoContentResult();
-        }
+//            await _filterRepository.Delete(id);
+//            return new NoContentResult();
+//        }
 
-        [HttpPut]
-        [Authorize("manage_account_filtering")]
-        public async Task<IActionResult> UpdateFilter([FromBody] UpdateFilterRequest updateFilterRequest)
-        {
-            if (updateFilterRequest == null)
-            {
-                return BuildMissingParameter(nameof(updateFilterRequest));
-            }
+//        [HttpPut]
+//        [Authorize("manage_account_filtering")]
+//        public async Task<IActionResult> UpdateFilter([FromBody] UpdateFilterRequest updateFilterRequest)
+//        {
+//            if (updateFilterRequest == null)
+//            {
+//                return BuildMissingParameter(nameof(updateFilterRequest));
+//            }
 
-            IActionResult errorResult = null;
-            if (!CheckParameter(updateFilterRequest, out errorResult))
-            {
-                return errorResult;
-            }
+//            IActionResult errorResult = null;
+//            if (!CheckParameter(updateFilterRequest, out errorResult))
+//            {
+//                return errorResult;
+//            }
 
-            var filter = await _filterRepository.Get(updateFilterRequest.Id);
-            if (filter == null)
-            {
-                return new StatusCodeResult((int)HttpStatusCode.NotFound);
-            }
+//            var filter = await _filterRepository.Get(updateFilterRequest.Id);
+//            if (filter == null)
+//            {
+//                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+//            }
 
-            await _filterRepository.Update(updateFilterRequest.ToParameter());
-            return new NoContentResult();
-        }
+//            await _filterRepository.Update(updateFilterRequest.ToParameter());
+//            return new NoContentResult();
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Private methods
+//        #region Private methods
 
-        private static bool CheckParameter(AddFilterRequest addFilterRequest, out IActionResult actionResult)
-        {
-            actionResult = null;
-            if (string.IsNullOrWhiteSpace(addFilterRequest.Name))
-            {
-                actionResult = BuildMissingParameter("name");
-                return false;
-            }
+//        private static bool CheckParameter(AddFilterRequest addFilterRequest, out IActionResult actionResult)
+//        {
+//            actionResult = null;
+//            if (string.IsNullOrWhiteSpace(addFilterRequest.Name))
+//            {
+//                actionResult = BuildMissingParameter("name");
+//                return false;
+//            }
 
-            return true;
-        }
+//            return true;
+//        }
 
-        private static bool CheckParameter(UpdateFilterRequest updateFilterRequest, out IActionResult actionResult)
-        {
-            actionResult = null;
-            if (string.IsNullOrWhiteSpace(updateFilterRequest.Id))
-            {
-                actionResult = BuildMissingParameter("id");
-                return false;
-            }
+//        private static bool CheckParameter(UpdateFilterRequest updateFilterRequest, out IActionResult actionResult)
+//        {
+//            actionResult = null;
+//            if (string.IsNullOrWhiteSpace(updateFilterRequest.Id))
+//            {
+//                actionResult = BuildMissingParameter("id");
+//                return false;
+//            }
 
-            if (string.IsNullOrWhiteSpace(updateFilterRequest.Name))
-            {
-                actionResult = BuildMissingParameter("name");
-                return false;
-            }
+//            if (string.IsNullOrWhiteSpace(updateFilterRequest.Name))
+//            {
+//                actionResult = BuildMissingParameter("name");
+//                return false;
+//            }
 
-            return true;
-        }
+//            return true;
+//        }
 
-        private static IActionResult BuildMissingParameter(string parameterName)
-        {
-            var error = new ErrorResponse
-            {
-                Error = "invalid_request",
-                ErrorDescription = string.Format("the parameter {0} is missing", parameterName)
-            };
+//        private static IActionResult BuildMissingParameter(string parameterName)
+//        {
+//            var error = new ErrorResponse
+//            {
+//                Error = "invalid_request",
+//                ErrorDescription = string.Format("the parameter {0} is missing", parameterName)
+//            };
 
-            return new JsonResult(error)
-            {
-                StatusCode = (int)HttpStatusCode.BadRequest
-            };
-        }
+//            return new JsonResult(error)
+//            {
+//                StatusCode = (int)HttpStatusCode.BadRequest
+//            };
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
