8e8b22730a58b5cb03d0c02bea0b9fece649422b
Jacob Reimers <jacob@reimers.dk>
2018-12-28 18:12:22 +0100

    Update file headers.

--- tests/SimpleIdentityServer.Scim.Client.Tests/GroupsClientFixture.cs
+++ tests/SimpleIdentityServer.Scim.Client.Tests/GroupsClientFixture.cs
@@ -1,4 +1,4 @@
-﻿//// Copyright 2016 Habart Thierry
+﻿//// Copyright © 2018 Habart Thierry, © 2018 Jacob Reimers
 //// 
 //// Licensed under the Apache License, Version 2.0 (the "License");
 //// you may not use this file except in compliance with the License.
