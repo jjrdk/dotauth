dbd9c0c79b7845d13a89a29ae0ec0d0c8842b1e0
Jacob Reimers <jacob@reimers.dk>
2019-02-24 18:56:32 +0100

    Fix tests.

--- src/simpleauth/Services/TwoFactorAuthenticationHandler.cs
+++ src/simpleauth/Services/TwoFactorAuthenticationHandler.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright © 2015 Habart Thierry, © 2018 Jacob Reimers
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,12 +14,12 @@
 
 namespace SimpleAuth.Services
 {
+    using Shared;
+    using Shared.Models;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using Shared;
-    using Shared.Models;
 
     internal class TwoFactorAuthenticationHandler : ITwoFactorAuthenticationHandler
     {
@@ -27,7 +27,9 @@ namespace SimpleAuth.Services
 
         public TwoFactorAuthenticationHandler(IEnumerable<ITwoFactorAuthenticationService> twoFactorServices)
         {
-            _twoFactorServices = twoFactorServices.ToArray();
+            _twoFactorServices = twoFactorServices == null
+                ? Array.Empty<ITwoFactorAuthenticationService>()
+                : twoFactorServices.ToArray();
         }
 
         public ITwoFactorAuthenticationService Get(string twoFactorAuthType)
