3cb168ede2ff5577b15825c3226fece97c6c9db9
Jacob Reimers <jacob@reimers.dk>
2019-03-21 21:21:51 +0100

    Test cleanup.

--- tests/simpleauth.tests/Api/Sms/Actions/SmsAuthenticationOperationFixture.cs
+++ tests/simpleauth.tests/Api/Sms/Actions/SmsAuthenticationOperationFixture.cs
@@ -14,13 +14,12 @@
 
     public class SmsAuthenticationOperationFixture
     {
-        private readonly Mock<IResourceOwnerRepository> _resourceOwnerRepositoryStub;
         private readonly SmsAuthenticationOperation _smsAuthenticationOperation;
 
         public SmsAuthenticationOperationFixture()
         {
             var generateAndSendSmsCodeOperationStub = new Mock<IConfirmationCodeStore>();
-            _resourceOwnerRepositoryStub = new Mock<IResourceOwnerRepository>();
+            var resourceOwnerRepositoryStub = new Mock<IResourceOwnerRepository>();
             var subjectBuilderStub = new Mock<ISubjectBuilder>();
             subjectBuilderStub.Setup(x => x.BuildSubject(It.IsAny<IEnumerable<Claim>>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(DateTime.UtcNow.Ticks.ToString);
@@ -28,7 +27,7 @@
                 new RuntimeSettings(),
                 null,
                 generateAndSendSmsCodeOperationStub.Object,
-                _resourceOwnerRepositoryStub.Object,
+                resourceOwnerRepositoryStub.Object,
                 subjectBuilderStub.Object,
                 new IAccountFilter[0],
                 new Mock<IEventPublisher>().Object);
@@ -41,24 +40,15 @@
                 .ThrowsAsync<ArgumentNullException>(
                     () => _smsAuthenticationOperation.Execute(null, CancellationToken.None))
                 .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task When_Empty_Parameter_Is_Passed_Then_Exception_Is_Thrown()
+        {
             await Assert
                 .ThrowsAsync<ArgumentNullException>(
                     () => _smsAuthenticationOperation.Execute(string.Empty, CancellationToken.None))
                 .ConfigureAwait(false);
-        }
-
-        [Fact(Skip = "Integration Test")]
-        public async Task When_ResourceOwner_Exists_Then_ResourceOwner_Is_Returned()
-        {
-            const string phone = "phone";
-            var resourceOwner = new ResourceOwner {Subject = "id"};
-            _resourceOwnerRepositoryStub
-                .Setup(p => p.GetResourceOwnerByClaim("phone_number", phone, CancellationToken.None))
-                .Returns(() => Task.FromResult(resourceOwner));
-
-            var result = await _smsAuthenticationOperation.Execute(phone, CancellationToken.None).ConfigureAwait(false);
-
-            Assert.Equal(resourceOwner.Subject, result.Subject);
         }
     }
 }
