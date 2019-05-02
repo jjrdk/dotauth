f67fa2bc4e0d56629e2827988aaa8f4ca622ff39
Jacob Reimers <jacob@reimers.dk>
2019-05-02 09:03:51 +0200

    Create rate limiter for sending sms codes.

---
+++ src/simpleauth.sms/NoopLimiter.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleAuth.Sms
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+
+    /// <inheritdoc />
+    public class NoopLimiter : IRateLimiter
+    {
+        /// <summary>
+        /// Returns the default instance of the <see cref="NoopLimiter"/>.
+        /// </summary>
+        public static IRateLimiter Default { get; } = new NoopLimiter();
+
+        /// <inheritdoc />
+        public Task<bool> Allow(HttpRequest request)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
