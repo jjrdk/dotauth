2a535038d477f42a435ae0915b1f078092911006
Sergio Brotons <sergio.brotons@roche.com>
2019-07-11 17:21:45 +0200

    Queries where pagination is involved in Simpleauth marten stores adjusted to count the total results correctly

--- src/simpleauth.stores.marten/MartenPolicyRepository.cs
+++ src/simpleauth.stores.marten/MartenPolicyRepository.cs
@@ -9,6 +9,8 @@
     using System.Threading;
     using System.Threading.Tasks;
 
+    using global::Marten.Pagination;
+
     /// <summary>
     /// Defines the Marten based policy repository.
     /// </summary>
@@ -34,15 +36,12 @@
             using (var session = _sessionFactory())
             {
                 var results = await session.Query<Policy>()
-                    .Where(
-                        x => x.Id.IsOneOf(parameter.Ids) || x.ResourceSetIds.Any(r => r.IsOneOf(parameter.ResourceIds)))
-                    .Skip(parameter.StartIndex)
-                    .Take(parameter.TotalResults)
-                    .ToListAsync(cancellationToken)
+                    .Where(x => x.Id.IsOneOf(parameter.Ids) || x.ResourceSetIds.Any(r => r.IsOneOf(parameter.ResourceIds)))
+                    .ToPagedListAsync(parameter.StartIndex, parameter.TotalResults, cancellationToken)
                     .ConfigureAwait(false);
                 return new GenericResult<Policy>
                 {
-                    Content = results.ToArray(), StartIndex = parameter.StartIndex, TotalResults = results.Count
+                    Content = results.ToArray(), StartIndex = parameter.StartIndex, TotalResults = results.TotalItemCount
                 };
             }
         }
