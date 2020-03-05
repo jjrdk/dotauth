d0c70c2be74bd7e3c9061c284ecf4fc45972d585
Jacob Reimers <jacob@reimers.dk>
2020-03-05 10:27:18 +0100

    Fix openid form_post authentication flow.

---
+++ src/simpleauth/Controllers/FormController.cs
@@ -0,0 +1,55 @@
+﻿// Copyright © 2015 Habart Thierry, © 2018 Jacob Reimers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace SimpleAuth.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SimpleAuth.ViewModels;
+
+    public class FormController : Controller
+    {
+        public ActionResult Index()
+        {
+            var queryStringValue = Request.QueryString.Value;
+            var queryString = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(queryStringValue);
+            var viewModel = new FormViewModel();
+            if (queryString.ContainsKey(StandardAuthorizationResponseNames.AccessTokenName))
+            {
+                viewModel.AccessToken = queryString[StandardAuthorizationResponseNames.AccessTokenName];
+            }
+
+            if (queryString.ContainsKey(StandardAuthorizationResponseNames.AuthorizationCodeName))
+            {
+                viewModel.AuthorizationCode = queryString[StandardAuthorizationResponseNames.AuthorizationCodeName];
+            }
+
+            if (queryString.ContainsKey(StandardAuthorizationResponseNames.IdTokenName))
+            {
+                viewModel.IdToken = queryString[StandardAuthorizationResponseNames.IdTokenName];
+            }
+
+            if (queryString.ContainsKey(StandardAuthorizationResponseNames.StateName))
+            {
+                viewModel.State = queryString[StandardAuthorizationResponseNames.StateName];
+            }
+
+            if (queryString.ContainsKey("redirect_uri"))
+            {
+                viewModel.RedirectUri = queryString["redirect_uri"];
+            }
+
+            return View(viewModel);
+        }
+    }
+}
