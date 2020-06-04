bdb2aab540c48b97fd45ad10d3511a3684d4d75d
Jacob Reimers <jacob@reimers.dk>
2020-06-04 20:26:48 +0200

    Fix storage of jwk with Marten. Allow for public/private version of keys in same table.

---
+++ src/simpleauth.stores.marten/JsonWebKeyContainer.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleAuth.Stores.Marten
+{
+    using System;
+    using Microsoft.IdentityModel.Tokens;
+
+    public class JsonWebKeyContainer
+    {
+        public string Id { get; set; }
+        public JsonWebKey Jwk { get; set; }
+
+        public static JsonWebKeyContainer Create(JsonWebKey key)
+        {
+            return new JsonWebKeyContainer
+            {
+                Id = Guid.NewGuid().ToString("N"),
+                Jwk = key
+            };
+        }
+    }
+}
