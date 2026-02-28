6e619cdc7cb607894ad6d9fcc3b22c773a1a4029
Jacob Reimers <jacob@reimers.dk>
2026-02-28 23:26:17 +0100

    Update Program.cs
    Update dependency

--- cake/dotauth.build/Program.cs
+++ cake/dotauth.build/Program.cs
@@ -3,6 +3,6 @@ using Cake.Frosting;
 using DotAuth.Build;
 
 return new CakeHost()
-    .InstallTool(new Uri("nuget:?package=GitVersion.Tool&version=5.12.0"))
+    .InstallTool(new Uri("nuget:?package=GitVersion.Tool&version=6.6.0"))
     .UseContext<BuildContext>()
     .Run(args);
